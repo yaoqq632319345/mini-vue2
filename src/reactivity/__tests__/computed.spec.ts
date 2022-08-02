71cfa7694b31304e110c8909be5688444fba4ec1
姚头 <632319345@qq.com>
2022-08-02 17:56:39 +0800

    简易computed

--- src/reactivity/__tests__/computed.spec.ts
+++ src/reactivity/__tests__/computed.spec.ts
@@ -9,4 +9,11 @@ describe('computed', () => {
     const c = computed(() => obj.foo);
     expect(c.value).toBe(100);
   });
+  it('lazy computed', () => {
+    const obj = reactive({
+      foo: 100,
+    });
+    const getter = () => obj.foo;
+    const c = computed(getter);
+  });
 });
