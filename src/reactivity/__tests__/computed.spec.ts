88b5460c0a7e8ed51b5ec65bf61d97f86ea48a6f
姚头 <632319345@qq.com>
2022-08-02 18:21:09 +0800

    lazy computed

--- src/reactivity/__tests__/computed.spec.ts
+++ src/reactivity/__tests__/computed.spec.ts
@@ -13,7 +13,27 @@ describe('computed', () => {
     const obj = reactive({
       foo: 100,
     });
-    const getter = () => obj.foo;
+    const getter = jest.fn(() => obj.foo);
     const c = computed(getter);
+    // 不取值不调用
+    expect(getter).not.toHaveBeenCalled();
+    // 取一次调一次
+    expect(c.value).toBe(100);
+    expect(getter).toHaveBeenCalledTimes(1);
+    // 值没变再取不该调
+    c.value;
+    expect(getter).toHaveBeenCalledTimes(1);
+
+    // 更新依赖值
+    obj.foo = 1000;
+    expect(getter).toHaveBeenCalledTimes(1);
+
+    // 再次调用，次数加一
+    expect(c.value).toBe(1000);
+    expect(getter).toHaveBeenCalledTimes(2);
+
+    // 值没变再次数还是2
+    c.value;
+    expect(getter).toHaveBeenCalledTimes(2);
   });
 });
--- src/reactivity/computed.ts
+++ src/reactivity/computed.ts
@@ -6,10 +6,17 @@ export function computed(fn: any): any {
 class ComputedImpl {
   private _effect;
   private _value;
+  private _dirty = true; // 记录一个值来确定什么时候更新
   constructor(fn) {
-    this._effect = new ReactiveEffect(fn, {});
+    this._effect = new ReactiveEffect(fn, {
+      scheduler: () => (this._dirty = true),
+    });
   }
   get value() {
-    return (this._value = this._effect.run());
+    if (this._dirty) {
+      this._value = this._effect.run();
+      this._dirty = false;
+    }
+    return this._value;
   }
 }
