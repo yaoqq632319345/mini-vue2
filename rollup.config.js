dd1359b256f6b42ba5b9b8b8dcde3e25a2e29659
姚头 <632319345@qq.com>
2022-08-03 18:14:47 +0800

    rollup

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json';
+
+export default {
+  input: './src/index.ts',
+  output: [
+    {
+      format: 'es',
+      file: pkg.module,
+    },
+    {
+      format: 'cjs',
+      file: pkg.main,
+    },
+  ],
+  plugins: [typescript()],
+};
